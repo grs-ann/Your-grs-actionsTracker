0154485bf5208286018f60bfa037c1afdb1cd18f
grs-ann <66564854+grs-ann@users.noreply.github.com>
2020-12-29 10:34:42 +0400

    Add project files.

---
+++ your-grs-actionsTracker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace your_grs_actionsTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
