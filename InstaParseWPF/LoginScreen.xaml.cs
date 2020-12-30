e85a751d35f688df91d7507fd46c529b6d5f9f20
grs-ann <66564854+grs-ann@users.noreply.github.com>
2020-12-30 23:14:55 +0400

    Added a WPF project. Added a new Login window and login logic.

---
+++ InstaParseWPF/LoginScreen.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using InstaParseWPF.InstagramTracker;
+
+namespace InstaParseWPF
+{
+    /// <summary>
+    /// Interaction logic for LoginScreen.xaml
+    /// </summary>
+    public partial class LoginScreen : Window
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+        private void buttonSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            Login.userLogin = txtUsername.Text;
+            // This is bad practice!!!!!!! But now it is not critically.
+            Login.userPassword = txtPassword.Text;
+            try
+            {
+                var result = Task.Run(Login.MainAsync).GetAwaiter().GetResult();
+                if (result)
+                {
+                    MessageBox.Show("Вход выполнен успешно.");
+                }
+                else
+                {
+                    MessageBox.Show("Что то пошло не так.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
